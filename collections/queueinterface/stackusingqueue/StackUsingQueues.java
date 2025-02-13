5bd48e01cdbb4cc66dfd1d456161b877d272d29c
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-13 10:49:58 +0530

    [Niraj] Add. Implemented stack using queue

---
+++ collections/queueinterface/stackusingqueue/StackUsingQueues.java
@@ -0,0 +1,63 @@
+package queueinterface.stackusingqueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingQueues {
+    private Queue<Integer> queue1 = new LinkedList<>();
+    private Queue<Integer> queue2 = new LinkedList<>();
+
+    public void push(int x) {
+        queue1.offer(x);
+    }
+
+    public int pop() {
+        if (queue1.isEmpty()) throw new RuntimeException("Stack is empty!");
+
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+
+        int topElement = queue1.poll();
+
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+
+        return topElement;
+    }
+
+    public int top() {
+        if (queue1.isEmpty()) throw new RuntimeException("Stack is empty!");
+
+        while (queue1.size() > 1) {
+            queue2.offer(queue1.poll());
+        }
+
+        int topElement = queue1.peek();
+        queue2.offer(queue1.poll());
+
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+
+        return topElement;
+    }
+
+    public boolean isEmpty() {
+        return queue1.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        StackUsingQueues stack = new StackUsingQueues();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+    }
+}
+

4a2f87cb41302b8fbb1d51e1248684c0dedc408f
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-13 10:41:31 +0530

    [Niraj] Add. Find key with highest value

---
+++ collections/mapinterface/keywithhighestvalue/KeyWithHighestValue.java
@@ -0,0 +1,27 @@
+package mapinterface.keywithhighestvalue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyWithHighestValue {
+    public static String findKeyWithMaxValue(Map<String, Integer> map) {
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+        return maxKey;
+    }
+    public static void main(String[] args) {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 10);
+        inputMap.put("B", 20);
+        inputMap.put("C", 15);
+
+        System.out.println(findKeyWithMaxValue(inputMap));
+    }
+}

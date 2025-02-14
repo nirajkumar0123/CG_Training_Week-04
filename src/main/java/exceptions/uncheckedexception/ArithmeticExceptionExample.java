75b9e5518c4fd83d5e4d0f8997fac04c96883e78
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-14 16:46:29 +0530

    [Niraj] Add. Implemented unchecked exception problem

---
+++ src/main/java/exceptions/uncheckedexception/ArithmeticExceptionExample.java
@@ -0,0 +1,25 @@
+package exceptions.uncheckedexception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ArithmeticExceptionExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter num1: ");
+            int num1 = sc.nextInt();
+
+            System.out.println("Enter num2: ");
+            int num2 = sc.nextInt();
+
+            int result = num1 / num2;
+            System.out.println("Result is: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Divison by zero is not allowed");
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Enter valid numeric value");
+        }
+        sc.close();
+    }
+}

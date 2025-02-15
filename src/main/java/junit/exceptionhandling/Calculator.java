e48ddb489c847e3c389152434c4920e44289b3ee
Niraj Kumar <nirajgupta7481@gmail.com>
2025-02-15 20:45:55 +0530

    [Niraj] Add. Exception Handling Program

---
+++ src/main/java/junit/exceptionhandling/Calculator.java
@@ -0,0 +1,11 @@
+package junit.exceptionhandling;
+
+public class Calculator {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
+
